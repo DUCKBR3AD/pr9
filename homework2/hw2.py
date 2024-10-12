7c2435a01b81bd4380d4ce54e5b044189cda9f68
DUCKBR3AD <76643973+DUCKBR3AD@users.noreply.github.com>
2024-10-12 13:18:51 +0500

    Полное исправление задания 2
    Исправлена ошибка, из за которой программа не работала при вводе вещественных чисел, также исправлена ошибка с некорректным вводом

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,32 @@
-a = int(input("a: "))
-b = int(input("b: "))
+import math
 
-list = []
+def float_input(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Введите корректное число.")
 
-for i in range(a+1,b):
-    i = i ** 2
-    list.append(i)
-  
-print(list)
+def squares(a, b):
+    start = math.ceil(min(a, b))
+    end = math.floor(max(a, b))
+
+    if a == start:
+        start += 1
+    if b == end:
+        end -= 1
+
+    return [i**2 for i in range(start, end + 1)]
+
+while True:
+    a = float_input("Введите число верхней границы: ")
+    b = float_input("Введите число нижней границы: ")
+
+    square_list = squares(a, b)
+
+    if square_list :
+        print(f"Квадраты целых чисел между {a} и {b}: {square_list}")
+        break
+    else:
+        print(f"между {a} и {b} нет целых чисел")
+        break
