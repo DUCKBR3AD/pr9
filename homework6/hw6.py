f640e9d0324b12e4c8da6e30305c23d88055b7e9
DUCKBR3AD <76643973+DUCKBR3AD@users.noreply.github.com>
2024-10-12 13:53:11 +0500

    Задание 6
    Пользователь вводит элементы списка через пробел, на выводе программа меняет первый и последний элемент списка местами и выводит его

---
+++ homework6/hw6.py
@@ -0,0 +1,9 @@
+def swap(lst):
+    min_index = lst.index(min(lst))
+    max_index = lst.index(max(lst))
+    lst[min_index], lst[max_index] = lst[max_index], lst[min_index]
+    return lst
+
+numbers = [int(x) for x in input("Введите список чисел через пробел: ").split()]
+numbers = swap(numbers)
+print(numbers)
