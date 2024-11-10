90046a7d14417045e4c92a1acc17c1a627833172
DUCKBR3AD <76643973+DUCKBR3AD@users.noreply.github.com>
2024-11-10 19:33:05 +0500

    Update hw9.py

--- homework9/hw9.py
+++ homework9/hw9.py
@@ -1,23 +1,21 @@
 import re
+while True:
+    def parse_email(email):
+        pattern = r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+        if re.match(pattern, email):
+            username, domain = email.split("@")
+            return username, domain
+        else:
+            return None, None
 
-def parse_email(email):
-    pattern = r"(.*)@(.*)"
-    match = re.match(pattern, email)
-    if match:
-        username = match.group(1)
-        domain = match.group(2)
-        return username, domain
-    else:
-        return None, None
+    def main():
+        email = input("Введите email: ")
+        username, domain = parse_email(email)
+        if username and domain:
+            print(f"Username: {username}")
+            print(f"Domain: {domain}")
+        else:
+            print("Некорректный email")
 
-def main():
-    email = input("Введите email: ")
-    username, domain = parse_email(email)
-    if username and domain:
-        print(f"Username: {username}")
-        print(f"Domain: {domain}")
-    else:
-        print("Некорректный email")
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
